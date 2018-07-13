9a656dbd602e15aa4d4442a164bcee1c835a62a4
felipemayer <felipecoelhomayer@gmail.com>
2018-07-13 13:24:14 -0300

    Changing Shebang and querys.

--- news_db.py
+++ news_db.py
@@ -1,4 +1,4 @@
-#! /usr/lib/python
+#! /usr/bin/env python
 import psycopg2
 
 # Connect database
@@ -8,7 +8,12 @@ conn = psycopg2.connect(database="news")
 c = conn.cursor()
 
 # Query for get the most view articles
-c.execute("select count(path) as sum, articles.title from log join articles on log.path = CONCAT('/article/', articles.slug) group by articles.title order by sum desc limit 3")  # noqa
+c.execute('''SELECT count(path) AS sum, articles.title 
+    FROM log 
+    JOIN articles ON log.path = CONCAT('/article/', articles.slug) 
+    GROUP BY articles.title 
+    ORDER BY sum desc 
+    LIMIT 3''')  # noqa
 results = c.fetchall()
 
 print("Most view articles:")
@@ -20,7 +25,12 @@ for item in results:
 
 print("\nLoading more data... \n")
 # Query for get the most popular authors
-c.execute("select count(path) as sum, authors.name from log join articles on log.path = CONCAT('/article/', articles.slug) join authors on articles.author = authors.id group by authors.name order by sum desc")  # noqa
+c.execute('''SELECT count(path) AS sum, authors.name 
+    FROM log 
+    JOIN articles ON log.path = CONCAT('/article/', articles.slug) 
+    JOIN authors ON articles.author = authors.id 
+    GROUP BY authors.name 
+    ORDER BY sum desc''')  # noqa
 results = c.fetchall()
 
 print("Most popular authors:")
@@ -32,9 +42,18 @@ for item in results:
 
 print("\nLoading more data... \n")
 # Query for getting days with more than 1% of error rate
-c.execute("CREATE or REPLACE VIEW error_view as SELECT time::date, count(status) as requests, status FROM log WHERE status != '200 OK' group by time::date, status")  # noqa
-c.execute("CREATE or REPLACE VIEW requests_view as SELECT time::date, count(status) as requests FROM log group by time::date")  # noqa
-c.execute("SELECT TO_CHAR(requests_view.time, 'Mon DD, YYYY'), error_view.requests / requests_view.requests::float as rate from requests_view, error_view where requests_view.time = error_view.time and error_view.requests / requests_view.requests::float >= 0.01")  # noqa
+c.execute('''CREATE or REPLACE VIEW error_view as 
+    SELECT time::date, count(status) as requests, status 
+    FROM log WHERE status != '200 OK' 
+    GROUP BY time::date, status''')  # noqa
+c.execute('''CREATE or REPLACE VIEW requests_view as 
+    SELECT time::date, count(status) as requests 
+    FROM log 
+    GROUP BY time::date''')  # noqa
+c.execute('''SELECT TO_CHAR(requests_view.time, 'Mon DD, YYYY'), error_view.requests / requests_view.requests::float as rate 
+    FROM requests_view, error_view 
+    WHERE requests_view.time = error_view.time 
+    AND error_view.requests / requests_view.requests::float >= 0.01''')  # noqa
 results = c.fetchall()
 
 print("Days with more than 1% of error rate:")
@@ -42,7 +61,7 @@ print("Days with more than 1% of error rate:")
 x = 0
 for item in results:
     error_rate = "{:.2%}".format(results[x][1])
-    print( results[x][0] + " - " + error_rate + " errors")
+    print(results[x][0] + " - " + error_rate + " errors")
     x = x + 1
 
 print("\nEnd of report.")
